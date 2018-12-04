728d64b3d145445f0ef8e71886117db1eea0220c
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-12-04 17:52:20 -0500

    function-earth first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/function-earth-spinning.png';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" /> 
+          <h1>COMING 2019</h1>
         </header>
+        
+        <div className="stars"></div>
+        <div className="twinkling"></div>
+        <div className="clouds"></div>
       </div>
     );
   }
