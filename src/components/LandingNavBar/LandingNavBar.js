03411a5e8f5f8c81ac62ebd3f469819fcca9d979
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-02-16 19:15:10 -0500

    added shadowed logo

--- src/components/LandingNavBar/LandingNavBar.js
+++ src/components/LandingNavBar/LandingNavBar.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './LandingNavBar.css';
-import logo from '../../assets/function-earth-logo2.png';
+import logo from '../../assets/function-earth-logo2-shadow.png';
 
 export class LandingNavBar extends Component {
 	render() {
 		return (
 			<div className="landing-nav-bar">
 				<div>
+
 					{/* <Link to="/dashboard"> */}
 						<img src={logo} className="landing-nav-bar-logo" alt="Think Programming Logo" />
 					{/* </Link> */}
